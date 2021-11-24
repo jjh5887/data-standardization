7e24b15266b8790090080ff9df3e1e51b39b4f6e
jjh5887 <wjdrnjsgh159@naver.com>
2021-11-24 16:13:12 +0900

    FWB-1 환경세팅

---
+++ src/test/java/kvoting/intern/flowerwebapp/FlowerWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package kvoting.intern.flowerwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

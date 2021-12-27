fa5b032c39010cb40c540f49d869431d36ac3102
sumin <hjssjh2322@naver.com>
2021-12-27 14:21:13 +0900

    FWF-3

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,9 +1,63 @@
-function infoPopup() {
-    const tableList = document.getElementById('tableList');
-    for (let i = 1; i < tableList.rows.length; i++) {
-        tableList.rows[i].cells[2].onclick = function () {
-            let tableList = tableList.rows[i].cells[1].innerText;
-            alert(tableList + "을 선택하셨습니다.");
-        }
+var input = document.getElementById("search");
+
+document.getElementById('btn1').onclick = function(){
+    let no, filter, tr, td, i, txtValue;
+    const select1 = document.getElementById('mySelect1');
+    const select2 = document.getElementById('mySelect2');
+
+    const table = Table_Const.find((item) => item.value === select1.value);
+    const target = table.name;
+
+    const word = Field_Const.WORD.find((item) => item.name === select2.value);
+    const target_word = word.value;
+
+    const params = {
+        pageNumber: 0,
+        pageSize: 20,
+        paged: true,
+        unpaged: false
     }
+    let query = Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])) .join('&');
+    const url = `http://localhost:8080/${target}/${target_word}/${input.value}?` + query + `&sort.sorted=false&sort.unsorted=true`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result.content.length);
+            let wordResult = document.querySelector('#word').innerHTML;
+            wordResult += '<tbody>'
+            for (let i = 0; i < result.content.length; i++) {
+                if (Object.keys(result.content[i]).includes('base')) {
+                    wordResult += '<tr onclick="">'
+                        + '<th>' + '<button name="T_Btn" type="button" class="btn-extension" onClick="">'
+                        + '상세보기' + '</button>'
+                        + ' </th>'
+                        + '<td>' + result.content[i].base?.engName + '</td>'
+                        + '<td>' + result.content[i].base?.name + '</td>'
+                        + '<td>' + result.content[i].base?.orgEngName + '</td>'
+                        + '<td>' + result.content[i].status + '</td>'
+                        + '</tr>';
+                }
+            }
+            wordResult += '</tbody>';
+            document.getElementById('word').innerHTML = wordResult;
+
+        });
+
+}
+input.onkeydown = function (event) {
+    if (event.keyCode == 13) {
+        document.getElementById("btn1").click();
+        event.preventDefault();
+    }
+
+}
+
+/* 페이징 */
+function pagination(){
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+
+
+        });
 }
